029522a9f1252ddaeefe6589ac20e3c1073c3a2f
Akram Darwazeh <a2.darwazeh@gmail.com>
2024-08-05 20:08:43 +1200

    feat: add typescript, redux, and create skelletons

--- Frontend-React/src/index.tsx
+++ Frontend-React/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { store } from './store/store'
+import { Provider } from 'react-redux'
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
 
