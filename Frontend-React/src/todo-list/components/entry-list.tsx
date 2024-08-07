c32550cd1c0d6b8c88cab11913e654cc4b959596
Akram Darwazeh <a2.darwazeh@gmail.com>
2024-08-07 23:42:08 +1200

    chore: remove initial code

--- Frontend-React/src/todo-list/components/entry-list.tsx
+++ Frontend-React/src/todo-list/components/entry-list.tsx
@@ -78,37 +78,4 @@ export const EntryList: FC<EntryListProps> = ({
       );
     })}
   </>
-
-  //     return <>
-  //     <h1>
-  //       Showing {items.length} Item(s){' '}
-  //       <Button variant="primary" className="pull-right" onClick={() => getItems()}>
-  //         Refresh
-  //       </Button>
-  //     </h1>
-
-
-  //     <Table striped bordered hover>
-  //       <thead>
-  //         <tr>
-  //           <th>Id</th>
-  //           <th>Description</th>
-  //           <th>Action</th>
-  //         </tr>
-  //       </thead>
-  //       <tbody>
-  //         {items.map((item) => (
-  //           <tr key={item.id}>
-  //             <td>{item.id}</td>
-  //             <td>{item.description}</td>
-  //             <td>
-  //               <Button variant="warning" size="sm" onClick={() => handleMarkAsComplete(item)}>
-  //                 Mark as completed
-  //               </Button>
-  //             </td>
-  //           </tr>
-  //         ))}
-  //       </tbody>
-  //     </Table>
-  //   </>
 }
